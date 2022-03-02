7749236ba45a36b05373f59e05dc5861713cb55e
Joseph <josephgabrieltina@gmail.com>
2022-03-02 14:53:07 +0800

    ui needs work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
-let toDateStr = "March 5, 2022 12:00:00"
-let toDate = new Date(toDateStr).getTime()
+let launchDateStr = "March 5, 2022 14:00:00"
+let launchDate = new Date(launchDateStr).getTime()
 
 const timer = document.getElementsByClassName('timer')
 
-setInterval(function(){
+function main(){
     update()
-}, 1000)
+    setTimeout(main, 1000) // loop each second
+}
 
+main()
 
 function update(){
-    let fromDate = new Date().getTime()
+    let currDate = new Date().getTime()
 
-    let timeLeft = toDate-fromDate // milliseconds
+    let timeLeft = launchDate-currDate // milliseconds
     
-    let daysLeft = ((((timeLeft / 1000) / 60) /60 )/ 24)
-    
-    timer[0].firstElementChild.innerHTML = Math.floor(daysLeft)
-    timer[1].firstElementChild.innerHTML = Math.floor(hoursLeft)
-    timer[2].firstElementChild.innerHTML = Math.floor(minutesLeft)
-    timer[3].firstElementChild.innerHTML = Math.floor(secondsLeft)
+    let daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+    let hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    let minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+    let secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000)
+
+    timer[0].firstElementChild.innerHTML = formatNum(daysLeft)
+    timer[1].firstElementChild.innerHTML = formatNum(hoursLeft)
+    timer[2].firstElementChild.innerHTML = formatNum(minutesLeft)
+    timer[3].firstElementChild.innerHTML = formatNum(secondsLeft)
+}
+
+function formatNum(digit){
+    if(digit < 10){
+        digit = '0'+digit
+    }
+    return digit
 }
