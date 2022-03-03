6584c9600b2681ca3e7b7520f7f714c4580e53c5
Joseph <josephgabrieltina@gmail.com>
2022-03-03 10:00:20 +0800

    added some responsiveness to ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-let launchDateStr = "March 5, 2022 14:00:00"
+let launchDateStr = "March 4, 2022 11:27:55"
 let launchDate = new Date(launchDateStr).getTime()
 
 const timer = document.getElementsByClassName('timer')
+const title = document.getElementsByClassName('title')
 
 function main(){
     update()
@@ -14,21 +15,30 @@ function update(){
     let currDate = new Date().getTime()
 
     let timeLeft = launchDate-currDate // milliseconds
-    
-    let daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
-    let hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    let minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
-    let secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000)
 
-    timer[0].firstElementChild.innerHTML = formatNum(daysLeft)
-    timer[1].firstElementChild.innerHTML = formatNum(hoursLeft)
-    timer[2].firstElementChild.innerHTML = formatNum(minutesLeft)
-    timer[3].firstElementChild.innerHTML = formatNum(secondsLeft)
+    if(timeLeft < 1){
+        title[0].firstElementChild.innerHTML = "Successful launch!"
+        for(let i = 0; i < timer.length; i++){
+            timer[i].firstElementChild.innerHTML = formatNum(0)
+            timer[i].firstElementChild.classList.remove('pink')
+            timer[i].firstElementChild.classList.toggle('blink')
+        }
+    }else{
+        let daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+        let hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        let minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+        let secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000)
+
+        timer[0].firstElementChild.innerHTML = formatNum(daysLeft)
+        timer[1].firstElementChild.innerHTML = formatNum(hoursLeft)
+        timer[2].firstElementChild.innerHTML = formatNum(minutesLeft)
+        timer[3].firstElementChild.innerHTML = formatNum(secondsLeft)
+    }
 }
 
 function formatNum(digit){
     if(digit < 10){
-        digit = '0'+digit
+        digit = '0' + digit
     }
     return digit
 }
